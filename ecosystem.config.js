53e865a3ac7609be0d41a14dba00de7f5dc82732
Rambo Lu <coderambolu@gmail.com>
2026-05-07 16:37:52 +0800

    feat: 添加配置文件并更新部署脚本

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+  apps: [{
+    name: 'info-collect',
+    script: 'server/index.js',
+    cwd: path.resolve(__dirname),
+    env: {
+      PORT: 3000,
+    },
+    instances: 1,
+    autorestart: true,
+    max_memory_restart: '256M',
+    log_date_format: 'YYYY-MM-DD HH:mm:ss',
+  }],
+}
